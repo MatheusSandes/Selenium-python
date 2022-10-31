163753062c699a838ed108f00cf3933b91f58c72
MatheusSandes <matheus_sandes@hotmail.com>
2022-10-31 14:39:34 -0300

    fix: edit the steps

--- test_login_and_register.py
+++ test_login_and_register.py
@@ -21,7 +21,7 @@ from pytest_bdd import (
     parsers
 )
 
-scenarios("./login_and_register.feature")
+scenarios("./login_and_register_scenarios.feature")
 from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
@@ -33,9 +33,9 @@ def generate_email():
         name = name + "".join(random.choice(string.ascii_lowercase))
     return name + "@gmail.com"
 
-def generate_cpf(self):                                                        
+def generate_cpf():                                                        
     cpf = [random.randint(0, 9) for x in range(9)]                              
-                                                                                
+                                                                               
     for _ in range(2):                                                          
         val = sum([(len(cpf) + 1 - i) * v for i, v in enumerate(cpf)]) % 11      
                                                                                 
@@ -49,44 +49,28 @@ def register_form(name, cpf, num, email, phone, password):
     driver.find_element(By.ID, ":r3:").click()
     driver.find_element(By.ID, ":r3:").send_keys(cpf)
     driver.find_element(By.ID, ":r4:").click()
-    driver.find_element(By.ID, ":r4:").send_keys(num) 
-    driver.find_element(By.ID, ":r5:").click()
+    driver.find_element(By.ID, ":r4:").send_keys(num)
     driver.find_element(By.CSS_SELECTOR, ".MuiButtonBase-root").click()
+    driver.find_element(By.ID, ":r5:").click()
     driver.find_element(By.ID, ":r5:").send_keys(email)
     driver.find_element(By.ID, ":r6:").click()
     driver.find_element(By.ID, ":r6:").send_keys(phone)
-    driver.find_element(By.ID, ":r7:").click()
     driver.find_element(By.CSS_SELECTOR, ".MuiButton-contained").click()
+    driver.find_element(By.ID, ":r7:").click()
     driver.find_element(By.ID, ":r7:").send_keys(password)
     driver.find_element(By.ID, ":r8:").click()
     driver.find_element(By.ID, ":r8:").send_keys(password)
     driver.find_element(By.CSS_SELECTOR, ".MuiButton-contained").click()
 
-@pytest.fixture()
 def setup():
     driver.get("https://proap-ufba.netlify.app/")
     driver.maximize_window()
 
-def add_wine_to_cart(number):
-    time.sleep(3)
-    i = 1
-    wait.until(
-            element_to_be_clickable((By.CLASS_NAME, 'ev-plus')),'Element not found'
-        )
-    while i < int(number):
-        driver.find_element(By.CLASS_NAME, 'ev-plus').click()
-        i+=1
-
-    element = driver.find_elements(By.XPATH, "//button[1]/div[2]") 
-    if len(element) > 0: 
-        driver.find_element(By.XPATH, "//button[1]/div[2]").click() 
-    else: 
-        driver.find_element(By.XPATH, "//div[contains(text(),'Adicionar')]").click()
-
 @given("user at register page")
 def user_at_register_page():
     """Add wines into cart"""
-    driver.find_element(By.LINK_TEXT, "cadastre-se").click()
+    setup()
+    driver.find_element(By.LINK_TEXT, "Cadastre-se").click()
 
 @when("fill the register form")
 def fill_the_register_form():
@@ -104,7 +88,6 @@ def fill_the_register_form_with_missing_fields():
     driver.find_element(By.ID, ":r3:").send_keys("")
     driver.find_element(By.ID, ":r4:").click()
     driver.find_element(By.ID, ":r4:").send_keys("1111")
-    driver.find_element(By.ID, ":r5:").click()
     driver.find_element(By.CSS_SELECTOR, ".MuiButtonBase-root").click()
 
 @when("fill the register form with invalid fields")
@@ -117,7 +100,6 @@ def fill_the_register_form_with_invalid_fields():
     driver.find_element(By.ID, ":r3:").send_keys("09876543210")
     driver.find_element(By.ID, ":r4:").click()
     driver.find_element(By.ID, ":r4:").send_keys("11111")
-    driver.find_element(By.ID, ":r5:").click()
     driver.find_element(By.CSS_SELECTOR, ".MuiButtonBase-root").click()
 
 @then("the user must be registered")
@@ -128,16 +110,18 @@ def the_user_must_be_registered():
     driver.find_element(By.ID, ":r1:").click()
     driver.find_element(By.ID, ":r1:").send_keys("0987654321")
     driver.find_element(By.CSS_SELECTOR, ".MuiButtonBase-root").click()
-    assert driver.find_element(By.XPATH, "//h4[contains(.,\'Acesso rápido\')]")
+    time.sleep(1)
+    assert driver.find_element(By.XPATH, "//h4[contains(.,\'Minhas solicitações\')]")
 
 @then("the system must show an error")
 def the_system_must_show_an_error():
-    n = driver.find_elements(By.XPATH, "//p[contains(.,'Campo obrigatório')]")
+    n = driver.find_elements(By.XPATH, "//p[contains(.,'Campo obrigatório')]").__len__()
     assert n == 2
 
 @then("the system must show an invalid field error")
-def the_system_must_show_an_error():
-    n = driver.find_elements(By.XPATH, "//p[contains(.,'CPF inválido')]")
+def the_system_must_show_an_invalid_field_error():
+    time.sleep(1)
+    n = driver.find_elements(By.XPATH, "//p[contains(.,'CPF inválido')]").__len__()
     assert n == 1
 
 @given("user at login page")
@@ -146,7 +130,7 @@ def user_at_login_page():
 
 @when("fill the login form as admin user")
 def fill_the_form_as_admin_user():
-    driver.find_element(By.NAME, "username").send_keys(email)
+    driver.find_element(By.NAME, "username").send_keys("matheus_sandes@hotmail.com")
     driver.find_element(By.NAME, "username").click()
     driver.find_element(By.NAME, "password").click()
     driver.find_element(By.NAME, "password").send_keys("0987654321")
@@ -155,4 +139,5 @@ def fill_the_form_as_admin_user():
 
 @then("user should be logged in as admin")
 def user_should_be_logged_in_as_admin():
-    assert driver.find_element(By.XPATH, "//h4[contains(.,\'Acesso rápido\')]")
+    time.sleep(2)
+    assert driver.find_element(By.XPATH, "//h4[contains(.,\'Minhas solicitações\')]")
